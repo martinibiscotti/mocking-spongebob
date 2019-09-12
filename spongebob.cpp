18a8338b04cc9bed909febe8ef12ab423897b8aa
Martin Biscocho <32799798+martinibiscotti@users.noreply.github.com>
2019-09-11 22:57:22 -0700

    added spongebob.cpp

---
+++ spongebob.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+  string sponge;
+  getline(cin, sponge);
+
+  for (unsigned i = 0; i < sponge.length(); ++i)
+  {
+    if (unsigned bob = (rand() & 1) == 0)
+      sponge[i] = tolower(sponge[i]);
+    else
+      sponge[i] = toupper(sponge[i]);
+  }
+
+  cout << sponge;
+
+  return 0;
+}

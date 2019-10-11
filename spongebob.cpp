2dd3170493b837e171b7c36e4a86e673995ae42c
Martin Biscocho <32799798+martinibiscotti@users.noreply.github.com>
2019-10-11 01:48:07 -0700

    added prompt

--- spongebob.cpp
+++ spongebob.cpp
@@ -5,6 +5,7 @@
 int main()
 {
   std::string sponge;
+  cout << "input: "
   getline(std::cin, sponge);
 
   for (int i = 0, bob = 0; i < sponge.length(); ++i, bob = rand() & 1)
@@ -12,7 +13,7 @@ int main()
     sponge[i] = bob ? tolower(sponge[i]) : toupper(sponge[i]);
   }
 
-  std::cout << sponge;
+  std::cout << "output: " << sponge;
 
   return 0;
 }
